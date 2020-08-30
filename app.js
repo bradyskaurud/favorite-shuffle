bba0323fdeb29d1cacbe63330319d43cfd797f7d
Brady Skaurud <bradyskaurud@gmail.com>
2020-08-29 22:29:39 -0500

    [CHORE] setup CORS

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ const spotifyRoutes = require('./routes/spotify');
 
 var app = express();
 
+// CORS config
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
